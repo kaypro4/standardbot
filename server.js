8bd8010374b263f871a7108d7056f69d83e643b5
Matt Pope <mpope@fairtradeusa.org>
2016-06-26 14:06:36 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,241 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+var wit = require('botkit-middleware-witai')({
+    token: process.env.WIT_TOKEN,
+});
+
+var Botkit = require('botkit');
+
+var MongoClient = require('mongodb').MongoClient, assert = require('assert');
+
+// Connection URL
+var url = process.env.MONGO_URL;
+
+
+var os = require('os');
+
+var controller = Botkit.slackbot({
+  debug: true
+});
+
+// connect the bot to a stream of messages
+controller.spawn({
+  token: process.env.SLACK_TOKEN,
+}).startRTM();
+
+controller.middleware.receive.use(wit.receive);
+
+controller.hears('howdy',['direct_message','direct_mention','mention'],wit.hears,function(bot,message) {
+
+  bot.reply(message,'Howdy yourself.');
+
+});
+
+
+controller.hears(['about (.*)', 'and (.*)'],['direct_message','direct_mention','mention'],function(bot,message) {
+  
+  var keyword = message.match[1];
+  
+  var attachments = [];
+  var fields = [];
+  var results;
+  
+  MongoClient.connect(url, function(err, db) {
+
+        var collection = db.collection('standards');
+
+        collection.find({tags: new RegExp(keyword)}).toArray(function(err, items) {
+          for (var i = 0; i < items.length; i++)
+          { 
+
+              fields.push({title: "Compliance #", value: items[i].compliance_no, short:true});
+              fields.push({title: "Criteria", value: items[i].criteria + '\n\n(' + items[i].tags + ')', short:true});
+          }
+          attachments.push({color: "#65CAE4", fields: fields});
+          
+          if (items.length > 0) {
+            results = {text: keyword + " eh? This what you were looking for?", attachments: attachments};
+          } else {
+            results = {text: "No results for " + keyword + ". Give something else a try. Perhaps women, equal, rest, etc."};
+          }
+          bot.reply(message, results);
+          
+        });
+  });
+  
+});
+
+controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', function(bot, message) {
+
+    bot.api.reactions.add({
+        timestamp: message.ts,
+        channel: message.channel,
+        name: 'robot_face',
+    }, function(err, res) {
+        if (err) {
+            bot.botkit.log('Failed to add emoji reaction :(', err);
+        }
+    });
+
+    controller.storage.users.get(message.user, function(err, user) {
+        if (user && user.name) {
+            bot.reply(message, 'Hello ' + user.name + '!!');
+        } else {
+            bot.reply(message, 'Hello, I\'m the standard bot. You can ask me things about the standards and I\'ll provide some info. You might try just asking - what about women?' );
+        }
+    });
+});
+
+controller.hears(['call me (.*)', 'my name is (.*)'], 'direct_message,direct_mention,mention', function(bot, message) {
+    var name = message.match[1];
+    controller.storage.users.get(message.user, function(err, user) {
+        if (!user) {
+            user = {
+                id: message.user,
+            };
+        }
+        user.name = name;
+        controller.storage.users.save(user, function(err, id) {
+            bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
+        });
+    });
+});
+
+controller.hears(['what is my name', 'who am i'], 'direct_message,direct_mention,mention', function(bot, message) {
+
+    controller.storage.users.get(message.user, function(err, user) {
+        if (user && user.name) {
+            bot.reply(message, 'Your name is ' + user.name);
+        } else {
+            bot.startConversation(message, function(err, convo) {
+                if (!err) {
+                    convo.say('I do not know your name yet!');
+                    convo.ask('What should I call you?', function(response, convo) {
+                        convo.ask('You want me to call you `' + response.text + '`?', [
+                            {
+                                pattern: 'yes',
+                                callback: function(response, convo) {
+                                    // since no further messages are queued after this,
+                                    // the conversation will end naturally with status == 'completed'
+                                    convo.next();
+                                }
+                            },
+                            {
+                                pattern: 'no',
+                                callback: function(response, convo) {
+                                    // stop the conversation. this will cause it to end with status == 'stopped'
+                                    convo.stop();
+                                }
+                            },
+                            {
+                                default: true,
+                                callback: function(response, convo) {
+                                    convo.repeat();
+                                    convo.next();
+                                }
+                            }
+                        ]);
+
+                        convo.next();
+
+                    }, {'key': 'nickname'}); // store the results in a field called nickname
+
+                    convo.on('end', function(convo) {
+                        if (convo.status == 'completed') {
+                            bot.reply(message, 'OK! I will update my dossier...');
+
+                            controller.storage.users.get(message.user, function(err, user) {
+                                if (!user) {
+                                    user = {
+                                        id: message.user,
+                                    };
+                                }
+                                user.name = convo.extractResponse('nickname');
+                                controller.storage.users.save(user, function(err, id) {
+                                    bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
+                                });
+                            });
+
+
+
+                        } else {
+                            // this happens if the conversation ended prematurely for some reason
+                            bot.reply(message, 'OK, nevermind!');
+                        }
+                    });
+                }
+            });
+        }
+    });
+});
+
+
+controller.hears(['shutdown'], 'direct_message,direct_mention,mention', function(bot, message) {
+
+    bot.startConversation(message, function(err, convo) {
+
+        convo.ask('Are you sure you want me to shutdown?', [
+            {
+                pattern: bot.utterances.yes,
+                callback: function(response, convo) {
+                    convo.say('Bye!');
+                    convo.next();
+                    setTimeout(function() {
+                        process.exit();
+                    }, 3000);
+                }
+            },
+        {
+            pattern: bot.utterances.no,
+            default: true,
+            callback: function(response, convo) {
+                convo.say('*Phew!*');
+                convo.next();
+            }
+        }
+        ]);
+    });
+});
+
+
+controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'],
+    'direct_message,direct_mention,mention', function(bot, message) {
+
+        var hostname = os.hostname();
+        var uptime = formatUptime(process.uptime());
+
+        bot.reply(message,
+            ':robot_face: I am a bot named <@' + bot.identity.name +
+             '>. I have been running for ' + uptime + ' on ' + hostname + '.');
+
+    });
+
+function formatUptime(uptime) {
+    var unit = 'second';
+    if (uptime > 60) {
+        uptime = uptime / 60;
+        unit = 'minute';
+    }
+    if (uptime > 60) {
+        uptime = uptime / 60;
+        unit = 'hour';
+    }
+    if (uptime != 1) {
+        unit = unit + 's';
+    }
+
+    uptime = uptime + ' ' + unit;
+    return uptime;
+}
